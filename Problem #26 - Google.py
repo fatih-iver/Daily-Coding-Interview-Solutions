fb451e73e81c2519beefe13ab0de2af1cdf25d37
Fatih İver <fatih.iver@boun.edu.tr>
2019-04-14 15:32:13 +0300

    Update Problem #26 - Google.py

--- Problem #26 - Google.py
+++ Problem #26 - Google.py
@@ -1,4 +1,4 @@
-"""
+ """
 This problem was asked by Google.
 
 Given a singly linked list and an integer k, remove the kth last element from the list. k is guaranteed to be smaller than the length of the list.
@@ -8,52 +8,27 @@ The list is very long, so making more than one pass is prohibitively expensive.
 Do this in constant space and in one pass.
 """
 
-class Node:
-  def __init__(self, val):
-    self.val = val
-    self.Next = None
+def remove_kth(ll, k):
 
-class LinkedList:
-  def __init__(self):
-    self.root = None
+  if k < 1:
+    return False
 
-  def insert(self, val):
-    new = Node(val)
-    new.Next = self.root
-    self.root = new
+  curr = ll.root
 
-  def traverse(self):
-    curr = self.root
-    while curr:
-      print(curr.val, end=" ")
-      curr = curr.Next
+  while curr and k > 0:
+    curr = curr.next
+    k -= 1
 
-
-ll = LinkedList()
-
-r = 1
-
-for _ in range(10):
-  r = (23*r + 17) % 11
-  ll.insert(r)
-
-ll.traverse()
- 
-class Smallest:
-  def initialize(self, root):
-    curr = root
-    while curr:
-      self.values.append(curr.val)
-      curr = curr.Next
-    self.values.sort()
-
-  def __init__(self, root):
-    self.values = []
-    self.initialize(root)
-
-  def get_kth_smallest(self, k):
-    return self.values[k-1]
-
-sma = Smallest(ll.root)  
-print()
-print(sma.get_kth_smallest(10))
+  if curr:
+    prev = ll.root
+    while curr.next:
+      prev = prev.next
+      curr = curr.next
+    prev.next = prev.next.next
+  else:
+    if k == 0:
+      ll.root = ll.root.next
+    else:
+      return False
+    
+  return True
